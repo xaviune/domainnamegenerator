a65f10b3d68b6afab04dd18f4b9377ec735a19d5
xaviune <xaviune@gmail.com>
2022-11-18 20:53:17 +0000

    sin terminar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generatedomain() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".es", ".net", ".us", ".io"];
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        for (let a = 0; a < domain.length; a++) {
+          console.log(pronoun[x] + adj[y] + noun[z] + domain[a]);
+        }
+      }
+    }
+  }
+}
