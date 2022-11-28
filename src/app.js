9f663a204e875de9f84017fa7664d19ca4badadb
xaviune <xaviune@gmail.com>
2022-11-28 14:02:07 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+window.onload = function() {
+  generatedomain();
+};
 function generatedomain() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
